9df548543359291e17eaa205297ca1d949ae17e5
Lapau5152 <phertre@gmail.com>
2026-04-18 16:35:46 -0600

    correcciones SEO

--- prueba/prueba/settings.py
+++ prueba/prueba/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-r6mv#il3=3hmb_(ny-qrjhd^tz*$jo(%%*i9$fz64u91vaa^q)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['paulina24.pythonanywhere.com']
 
 
 # Application definition
